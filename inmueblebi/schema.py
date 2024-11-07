0221261aff4159fbbfe9ed574e2b8cc9a0af71d9
gstevenvalverde <gstevenvalverde@gmail.com>
2024-11-07 00:10:30 -0400

    Análisis completo. Falta aplicar estilos css para presentar

--- inmueblebi/schema.py
+++ inmueblebi/schema.py
@@ -30,8 +30,13 @@ class PropiedadType(DjangoObjectType):
             "city",
             "zone",
         )
+
 class PrecioPromedioPorZonaYCiudadType(ObjectType):
     zona = String()
+    ciudad = String()
+    precio_promedio_por_m2 = Float()
+
+class PrecioPromedioPorCiudadType(ObjectType):
     ciudad = String()
     precio_promedio_por_m2 = Float()
 
@@ -49,18 +54,45 @@ class PromedioTiempoMercadoPorZonaYCiudadType(ObjectType):
     zona = String()
     promedio_dias_en_venta = Float()
 
+class PromedioTiempoMercadoPorCiudadType(ObjectType):
+    ciudad = String()
+    promedio_dias_en_venta = Int()
+
 class CantidadPropiedadesPorCiudadYZonaType(ObjectType):
     ciudad = String()
     zona = String()
     cantidad_propiedades = Int()
 
+class PropiedadesVendidasPorZoneType(ObjectType):
+    zona = String()
+    vendidos = Int()
+    no_vendidos = Int()
+
+class PrecioPromedioPorZonaType(ObjectType):
+    zona = String()
+    precio_promedio_por_m2 = Float()
+
+class PromedioTiempoMercadoPorZonaType(ObjectType):
+    zona = String()
+    promedio_dias_en_venta = Float()
+
+class ZoneType(ObjectType):
+    zone = String()
+
 class Query(graphene.ObjectType):
     propiedades = graphene.List(PropiedadType)
     calcular_precio_promedio_por_zona_ciudad = List(PrecioPromedioPorZonaYCiudadType)
+    calcular_precio_promedio_por_ciudad = List(PrecioPromedioPorCiudadType)
     obtener_datos_para_google_charts = List(GoogleChartsRowType)
     calcular_tasa_conversion_por_ciudad = List(TasaConversionPorCiudadType)
     calcular_promedio_tiempo_mercado_por_zona_y_ciudad = List(PromedioTiempoMercadoPorZonaYCiudadType)
+    calcular_promedio_tiempo_mercado_por_ciudad = List(PromedioTiempoMercadoPorCiudadType)
     calcular_cantidad_propiedades_por_ciudad_y_zona = List(CantidadPropiedadesPorCiudadYZonaType)
+
+    propiedades_vendidas_por_zona = graphene.List(PropiedadesVendidasPorZoneType, zone=graphene.String(required=True))
+    precio_m2_por_zona = graphene.List(PrecioPromedioPorZonaType, zone=graphene.String(required=True))
+    calcular_promedio_tiempo_mercado_por_zona = graphene.List(PromedioTiempoMercadoPorZonaType, zone=graphene.String(required=True))
+    obtener_zonas_unicas = graphene.List(ZoneType)
 
     def resolve_propiedades(self, info):
         return Propiedad.objects.all()
@@ -90,6 +122,32 @@ class Query(graphene.ObjectType):
         ]
 
         return result
+
+    def resolve_calcular_precio_promedio_por_ciudad(self, info):
+        # Consulta SQL para cargar los datos desde PostgreSQL
+        query = "SELECT price, sqm_living, city FROM inmueblesapp_propiedad"
+        data_frame = pd.read_sql_query(query, connection)
+
+        # Evitar divisiones por cero
+        data_frame = data_frame[data_frame['sqm_living'] > 0]
+
+        # Calcular el precio por metro cuadrado para cada propiedad
+        data_frame['precio_por_m2'] = data_frame['price'] / data_frame['sqm_living']
+
+        # Agrupar por zona y ciudad y calcular el precio por m2 promedio
+        grouped_data = data_frame.groupby(['city'])['precio_por_m2'].mean().reset_index()
+
+        # Convertir los resultados en una lista de objetos que GraphQL pueda devolver
+        result = [
+            {
+                'ciudad': row['city'],
+                'precio_promedio_por_m2': row['precio_por_m2']
+            }
+            for _, row in grouped_data.iterrows()
+        ]
+
+        return result
+
 
     def resolve_obtener_datos_para_google_charts(self, info):
         # Consulta SQL para cargar los datos desde PostgreSQL
@@ -175,6 +233,35 @@ class Query(graphene.ObjectType):
 
         return result
 
+    def resolve_calcular_promedio_tiempo_mercado_por_ciudad(self, info):
+        # Consulta SQL para cargar los datos desde PostgreSQL
+        query = "SELECT date_published, date_sold, city FROM inmueblesapp_propiedad WHERE date_sold IS NOT NULL"
+        data_frame = pd.read_sql_query(query, connection)
+
+        # Convertir las columnas de fecha a tipo datetime
+        data_frame['date_published'] = pd.to_datetime(data_frame['date_published'])
+        data_frame['date_sold'] = pd.to_datetime(data_frame['date_sold'])
+
+        # Calcular la diferencia en días entre la fecha de venta y la fecha de publicación
+        data_frame['dias_en_venta'] = (data_frame['date_sold'] - data_frame['date_published']).dt.days
+
+        # Agrupar por ciudad y zona y calcular el promedio de días en venta
+        grouped_data = data_frame.groupby(['city'])['dias_en_venta'].mean().reset_index()
+
+        # Entero del promedio de días en venta
+        grouped_data['dias_en_venta'] = grouped_data['dias_en_venta'].astype(int)
+
+        # Convertir los resultados en una lista de objetos que GraphQL pueda devolver
+        result = [
+            PromedioTiempoMercadoPorCiudadType(
+                ciudad=row['city'],
+                promedio_dias_en_venta=row['dias_en_venta']
+            )
+            for _, row in grouped_data.iterrows()
+        ]
+
+        return result
+
     def resolve_calcular_cantidad_propiedades_por_ciudad_y_zona(self, info):
         # Consulta SQL para cargar los datos desde PostgreSQL
         query = "SELECT city, zone FROM inmueblesapp_propiedad"
@@ -192,6 +279,87 @@ class Query(graphene.ObjectType):
             )
             for _, row in grouped_data.iterrows()
         ]
+
+        return result
+
+    def resolve_propiedades_vendidas_por_zona(self, info, zone):
+        # Obtén la cantidad de propiedades vendidas y no vendidas para la zona solicitada
+        total_vendidos = Propiedad.objects.filter(zone=zone, date_sold__isnull=False).count()
+        total_no_vendidos = Propiedad.objects.filter(zone=zone, date_sold__isnull=True).count()
+
+        # Estructura los datos en el formato solicitado
+        result = [PropiedadesVendidasPorZoneType(
+                zona = zone,
+                vendidos = total_vendidos,
+                no_vendidos= total_no_vendidos,
+            )]
+
+        return result
+
+    def resolve_precio_m2_por_zona(self, info, zone):
+        # Extrae las propiedades en la zona específica
+        propiedades = Propiedad.objects.filter(zone=zone).values('zone', 'price', 'sqm_living')
+
+        # Convierte los resultados a un DataFrame de Pandas
+        df = pd.DataFrame(list(propiedades))
+
+        # Verifica que el DataFrame no esté vacío
+        if df.empty:
+            return PrecioPromedioPorZonaType(zona=zone, precio_promedio_por_m2=None)
+
+        # Filtra filas sin valores de `sqm_living` o `price`
+        df = df[(df['sqm_living'] > 0) & (df['price'] > 0)]
+
+        # Calcula el precio promedio por metro cuadrado
+        df['precio_por_m2'] = df['price'] / df['sqm_living']
+        precio_promedio_por_m2 = round(df['precio_por_m2'].mean(), 2)
+
+        # Estructura los datos en el formato solicitado
+        result = [PrecioPromedioPorZonaType(
+            zona=zone,
+            precio_promedio_por_m2=precio_promedio_por_m2
+        )]
+        return result
+
+    def resolve_calcular_promedio_tiempo_mercado_por_zona(self, info, zone):
+        # Consulta SQL para cargar los datos de propiedades vendidas de la zona especificada
+        query = f"""
+        SELECT date_published, date_sold, zone 
+        FROM inmueblesapp_propiedad 
+        WHERE date_sold IS NOT NULL AND zone = %s
+        """
+        data_frame = pd.read_sql_query(query, connection, params=[zone])
+
+        # Verifica que haya datos en la zona especificada
+        if data_frame.empty:
+            return []
+
+        # Convertir las columnas de fecha a tipo datetime
+        data_frame['date_published'] = pd.to_datetime(data_frame['date_published'])
+        data_frame['date_sold'] = pd.to_datetime(data_frame['date_sold'])
+
+        # Calcular la diferencia en días entre la fecha de venta y la fecha de publicación
+        data_frame['dias_en_venta'] = (data_frame['date_sold'] - data_frame['date_published']).dt.days
+
+        # Calcular el promedio de días en venta para la zona
+        promedio_dias_en_venta = round(data_frame['dias_en_venta'].mean(), 2)
+
+        # Convertir los resultados en una lista de objetos que GraphQL pueda devolver
+        result = [
+            PromedioTiempoMercadoPorZonaType(
+                zona=zone,
+                promedio_dias_en_venta=promedio_dias_en_venta  # Redondear a dos decimales
+            )
+        ]
+
+        return result
+
+    def resolve_obtener_zonas_unicas(self, info):
+        # Realiza la consulta para obtener zonas únicas desde la base de datos
+        zonas_unicas = Propiedad.objects.values('zone').distinct()
+
+        # Convertir el resultado a una lista de objetos de tipo ZoneType
+        result = [ZoneType(zone=z['zone']) for z in zonas_unicas]
 
         return result
 
